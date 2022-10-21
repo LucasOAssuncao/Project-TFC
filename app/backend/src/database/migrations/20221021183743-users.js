3cfc9a3584ede7537d8d5f23f85c31b9f1f808b5
Lucas Otoni <lotoni.vivo@gmail.com>
2022-10-21 17:28:28 -0300

    fix: mudança de nome de arquivo

--- app/backend/src/database/migrations/20221021183743-users.js
+++ app/backend/src/database/migrations/20221021183743-users.js
@@ -1,13 +1,34 @@
+'use-strict';
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('trybe_eval', {
+    await queryInterface.createTable('users', {
       id: {
+        allowNull: false,
+        autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
       }
     });
   },
-  down: async (queryInterface) => {
-    await queryInterface.dropTable('trybe_eval');
-  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('users')
+  }
 };
