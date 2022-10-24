a7b8cfc1fb5a9bfcfe2087111ceb5556d4dc47eb
Lucas Otoni <lotoni.vivo@gmail.com>
2022-10-24 16:54:45 -0300

    feature: modificação para parameto no metodo get

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -2,10 +2,18 @@ import { Request, Response } from 'express';
 import MatchesService from '../services/MatchesService';
 
 export default class MatchesController {
-  constructor(private matchesService = new MatchesService()) { }
+  constructor(private matchesService = new MatchesService()) {}
 
   getAll = async (req: Request, res: Response) => {
+    const { inProgress } = req.query;
+
+    if (inProgress) {
+      const filteredMatches = await this.matchesService.getAll({ where: { inProgress: true } });
+
+      return res.status(200).json(filteredMatches);
+    }
     const allMatches = await this.matchesService.getAll();
+
     return res.status(200).json(allMatches);
   };
 }
--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -2,13 +2,18 @@ import Matches from '../database/models/matches';
 import Team from '../database/models/teams';
 // import { IMatches } from '../interfaces/IMatches';
 
+type IParam<T> = {
+  [key: string]: T;
+};
+
 export default class MatchesService {
-  getAll = async (): Promise<Matches[] | []> => {
+  getAll = async <T>(param?: IParam<T>): Promise<Matches[] | []> => {
     const allMatches = Matches.findAll({
       include: [
         { model: Team, as: 'teamHome', attributes: ['teamName'] },
         { model: Team, as: 'teamAway', attributes: ['teamName'] },
       ],
+      ...param,
     });
 
     return allMatches;
